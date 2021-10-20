c7d0b343612173617051c90d4fab43f7eeecaf24
Grisha Schuhmacher <g.schuhmacher@rafisa.ch>
2021-10-20 18:29:46 +0200

    FEAT: Lock the app to landscape mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:schuhmacher_itri_bma/app.dart';
 
 void main() {
-  runApp(const App());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeRight])
+      .then((_) {
+    runApp(const App());
+  });
 }
