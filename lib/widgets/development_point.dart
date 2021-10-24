671d4c6eb134a6dcec218a472fadcf37595d8bd9
Grisha Schuhmacher <g.schuhmacher@rafisa.ch>
2021-10-24 20:45:21 +0200

    FEAT: Implement rough animation

--- lib/widgets/development_point.dart
+++ lib/widgets/development_point.dart
@@ -7,19 +7,36 @@ class DevelopmentPoint extends StatefulWidget {
   _DevelopmentPointState createState() => _DevelopmentPointState();
 }
 
-class _DevelopmentPointState extends State<DevelopmentPoint> {
+class _DevelopmentPointState extends State<DevelopmentPoint>
+    with TickerProviderStateMixin {
+  bool _isLarge = false;
+  double _size = 20;
+
+  void _updateSize() {
+    setState(() {
+      _isLarge = !_isLarge;
+      _size = _isLarge ? 40 : 20;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 20,
-      width: 20,
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.primary,
-        border: Border.all(
-          color: Theme.of(context).colorScheme.onPrimary,
-          width: 3,
+    return GestureDetector(
+      onTap: _updateSize,
+      child: AnimatedSize(
+        duration: const Duration(seconds: 1),
+        child: Container(
+          height: _size,
+          width: _size,
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.primary,
+            border: Border.all(
+              color: Theme.of(context).colorScheme.onPrimary,
+              width: 3,
+            ),
+            borderRadius: BorderRadius.circular(50),
+          ),
         ),
-        borderRadius: BorderRadius.circular(10),
       ),
     );
   }
