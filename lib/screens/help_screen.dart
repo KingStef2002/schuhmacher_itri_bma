be6c57f0fa73ca2e7c9f57363225a22ea0dbd44f
ashirg-ch <ashirg@habmalnefrage.de>
2021-10-29 16:49:41 +0200

    FEAT: Implement provisional help screen

--- lib/screens/help_screen.dart
+++ lib/screens/help_screen.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
+// TODO: Finish the help screen
 class HelpScreen extends StatelessWidget {
+  static const route = '/help';
+
   const HelpScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hilfe'),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                'Verwende Wisch- und Ziebewegungen, um den Zeitstrahl zu navigieren.',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
